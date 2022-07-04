a33676b77d4ac75c671b792c12be72205cd9a337
Ashley <ashl3yaoa@gmail.com>
2022-07-04 11:30:23 +0000

    updated text in home.html and service 2 route

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -3,7 +3,7 @@ from random import choice
 
 @app.route('/get_text', methods=['GET'])
 def get_text():
-        alphabet ='abcdefghijklmnopqrstuvwxyz'
-        text = ''.join(choice(alphabet) for _ in range(3)) 
+        Prefix =["Lil","Baby","Da","Music","Soul","King","Prince","Childish","Tiny","Neo","Royal","Magic","Lyrical","Crooked","Slum","Machine","JAY","A","B"]
+        text = ''.join(choice(Prefix) for _ in range(1)) 
     
         return text 
