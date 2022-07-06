d9d00a89a337109f69a922bcea57c6fa54ff1c8e
Ashley <ashl3yaoa@gmail.com>
2022-07-06 09:07:20 +0000

    making sure its commited

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -3,7 +3,7 @@ from random import choice
 
 @app.route('/get_text', methods=['GET'])
 def get_text():
-        Prefix =["Lil","Baby","Da","Music","Soul","King","Prince","Childish","Tiny","Neo","Royal","Magic","Lyrical","Crooked","Slum","Machine","JAY","A","B"]
-        text = ''.join(choice(Prefix) for _ in range(1)) 
+        prefix =["Lil","Baby","Da","Music","Soul","King","Prince","Childish","Tiny","Neo","Royal","Magic","Lyrical","Crooked","Slum","Machine","JAY","A","B"]
+        text = ''.join(choice(prefix) for _ in range(1)) 
     
         return text 
