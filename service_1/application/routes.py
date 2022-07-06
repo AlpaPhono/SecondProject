7d4722626e79c8f5acd1d5da7f25a8b7b568fbcf
Ashley <ashl3yaoa@gmail.com>
2022-07-06 09:20:14 +0000

    added a link to service_3 in service_1, adjusted changes in the html template

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -4,6 +4,6 @@ import requests # seperate library aside from the flask requests
 
 @app.get('/')
 def index():
-    chars = requests.get('http://service_2:5000/get_text').text
-    prize = requests.get('http://service_4:5000/prize', data = chars)
-    return render_template('home.html', chars = chars, prize = prize.text) 
+    prefix = requests.get('http://service_2:5000/get_text').text
+    prize = requests.get('http://service_4:5000/prize', data = prefix)
+    return render_template('home.html', prefix = prefix, prize = prize.text) 
